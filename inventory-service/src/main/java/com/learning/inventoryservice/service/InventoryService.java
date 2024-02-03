bc583756a8e5f86c6f048cd8e7980530951df448
manishjain2503199 <manishkumar25031999@gmail.com>
2024-02-03 17:21:45 +0530

    zipkin done

--- inventory-service/src/main/java/com/learning/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/learning/inventoryservice/service/InventoryService.java
@@ -2,6 +2,8 @@ package com.learning.inventoryservice.service;
 
 import com.learning.inventoryservice.dto.InventoryResponse;
 import com.learning.inventoryservice.repo.InventoryRepo;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,14 +11,18 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
+@Slf4j
 public class InventoryService {
 
     @Autowired
    private InventoryRepo inventoryRepo ;
 
     @Transactional(readOnly = true)
-    public List<InventoryResponse> isInStock(List<String> skuCode)
-    {
+    @SneakyThrows
+    public List<InventoryResponse> isInStock(List<String> skuCode)  {
+        log.info("Wait Started");
+        Thread.sleep(10000);
+        log.info("Wait Ended");
         return inventoryRepo.findBySkuCodeIn(skuCode).stream()
                 .map(inventory -> InventoryResponse.builder()
                         .skuCode(inventory.getSkuCode())
